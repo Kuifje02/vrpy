417898b96c20c1757b64290a1a007a8498cf7f69
David Torres Sánchez <23246013+torressa@users.noreply.github.com>
2020-04-01 18:41:36 +0200

    Git clone cspy for ci and added tests

--- tests/test_cg.py
+++ tests/test_cg.py
@@ -4,10 +4,26 @@ sys.path.append("../vrpy")
 from vrpy.vrpy import main
 
 
-def test_main():
+def test_missing_node():
     """Tests column generation procedure on toy graph"""
     G = main.create_graph()
     G.remove_node(5)
     initial_routes = main.initialize_routes(G)
     best_value = main.main(G, initial_routes)
     assert best_value == 60
+
+
+def test_sub_cspy():
+    """Tests column generation procedure on toy graph"""
+    G = main.create_graph()
+    initial_routes = main.initialize_routes(G)
+    best_value = main.main(G, initial_routes, cspy=True)
+    assert best_value == 80
+
+
+def test_sub_pulp():
+    """Tests column generation procedure on toy graph"""
+    G = main.create_graph()
+    initial_routes = main.initialize_routes(G)
+    best_value = main.main(G, initial_routes, max_load=True)
+    assert best_value == 80
