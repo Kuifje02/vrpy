58895cb2ddce08c132535b74c00655ae9f88550a
Romain MONTAGNÉ <romain.montagne@fr.eurodecision.com>
2020-04-02 12:09:27 +0200

    update conf

--- docs/conf.py
+++ docs/conf.py
@@ -32,6 +32,7 @@ release = "0.1"
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = []
+master_doc = "index"
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
@@ -53,4 +54,3 @@ html_theme = "alabaster"
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
-
