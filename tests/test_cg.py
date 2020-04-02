392c84ef04061676dc117eabf4bc31f0ab950ea6
Romain MONTAGNÉ <romain.montagne@fr.eurodecision.com>
2020-04-02 13:01:02 +0200

    unit test with cspy

--- tests/test_cg.py
+++ tests/test_cg.py
@@ -4,10 +4,19 @@ sys.path.append("../vrpy")
 from vrpy.vrpy import main
 
 
-def test_main():
-    """Tests column generation procedure on toy graph"""
+def test_main_lp():
+    """Tests column generation procedure on toy graph with LP"""
     G = main.create_graph()
     G.remove_node(5)
     initial_routes = main.initialize_routes(G)
     best_value = main.main(G, initial_routes)
+    assert best_value == 60
+
+
+def test_main_cspy():
+    """Tests column generation procedure on toy graph with cspy"""
+    G = main.create_graph()
+    G.remove_node(5)
+    initial_routes = main.initialize_routes(G)
+    best_value = main.main(G, initial_routes, cspy=True)
     assert best_value == 60
