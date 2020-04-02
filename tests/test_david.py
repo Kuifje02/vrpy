1fd8bd1f6b80ad521daded9738bb66cd7cf62823
Romain MONTAGNÉ <romain.montagne@fr.eurodecision.com>
2020-04-02 15:46:11 +0200

    merge with master

---
+++ tests/test_david.py
@@ -0,0 +1,83 @@
+from networkx import DiGraph
+import sys
+
+sys.path.append("../vrpy")
+from vrpy.vrpy import main
+
+
+class TestsToy:
+    def setup(self):
+        """
+        Creates a toy graph and sets the initial routes for first iteration
+        """
+        self.G = DiGraph()
+        for v in [1, 2, 3, 4, 5]:
+            self.G.add_edge("Source", v, cost=10, time=20)
+            self.G.add_edge(v, "Sink", cost=10, time=20)
+            self.G.nodes[v]["demand"] = 5
+            self.G.nodes[v]["upper"] = 100
+            self.G.nodes[v]["lower"] = 0
+        self.G.nodes["Sink"]["demand"] = 0
+        self.G.nodes["Sink"]["lower"] = 0
+        self.G.nodes["Sink"]["upper"] = 100
+        self.G.nodes["Source"]["demand"] = 0
+        self.G.nodes["Source"]["lower"] = 0
+        self.G.nodes["Source"]["upper"] = 100
+        self.G.add_edge(1, 2, cost=10, time=20)
+        self.G.add_edge(2, 3, cost=10, time=20)
+        self.G.add_edge(3, 4, cost=15, time=20)
+        self.G.add_edge(4, 5, cost=10, time=25)
+        # Create routes
+        self.initial_routes = []
+        for v in self.G.nodes():
+            if v not in ["Source", "Sink"]:
+                route = DiGraph(name=v, cost=20)
+                route.add_edge("Source", v, cost=10)
+                route.add_edge(v, "Sink", cost=10)
+                self.initial_routes.append(route)
+
+    def test_missing_node(self):
+        """Tests column generation procedure on toy graph"""
+        self.G.remove_node(5)
+        best_value = main.main(
+            self.G, self.initial_routes, num_stops=4, load_capacity=10
+        )
+        assert best_value == 65
+
+    def test_sub_cspy(self):
+        """Tests column generation procedure on toy graph"""
+        best_value = main.main(
+            self.G, self.initial_routes, cspy=True, num_stops=4, load_capacity=10
+        )
+        assert best_value == 80
+
+    def test_LP_stops(self):
+        """Tests column generation procedure on toy graph"""
+        best_value = main.main(self.G, self.initial_routes, num_stops=4)
+        assert best_value == 70
+
+    def test_LP_stops_capacity(self):
+        """Tests column generation procedure on toy graph"""
+        best_value = main.main(
+            self.G, self.initial_routes, num_stops=4, load_capacity=10
+        )
+        assert best_value == 80
+
+    def test_LP_stops_capacity_duration(self):
+        """Tests column generation procedure on toy graph"""
+        best_value = main.main(
+            self.G, self.initial_routes, num_stops=4, load_capacity=10, duration=60
+        )
+        assert best_value == 85
+
+    def test_LP_stops_capacity_duration_time_windows(self):
+        """Tests column generation procedure on toy graph"""
+        best_value = main.main(
+            self.G,
+            self.initial_routes,
+            num_stops=4,
+            load_capacity=10,
+            duration=60,
+            time_windows=True,
+        )
+        assert best_value == 80
