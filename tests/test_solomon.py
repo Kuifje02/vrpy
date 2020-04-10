167227e265225c7e76dc538c7009f5ebe63e3b3d
Romain MONTAGNÉ <romain.montagne@fr.eurodecision.com>
2020-04-10 17:17:13 +0200

    temporarily commented solomon non reg test (too long)

--- tests/test_solomon.py
+++ tests/test_solomon.py
@@ -9,19 +9,18 @@ from examples.Solomon import DataSet
 
 
 class TestsSolomon:
-
     def setup(self):
         """
         Solomon instance c101, 25 first nodes only including depot
         """
-        self.data = DataSet(path="../examples/data/",
-                            instance_name="c101.txt",
-                            n_vertices=25)
+        self.data = DataSet(
+            path="../examples/data/", instance_name="c101.txt", n_vertices=25
+        )
         self.G = self.data.G
         self.n_vertices = 25
 
     def test_setup_instance_name(self):
-        assert self.G.graph["name"] == "c101"
+        assert self.G.graph["name"] == "c101." + str(self.n_vertices)
 
     def test_setup_vehicle_capacity(self):
         assert self.G.graph["vehicle_capacity"] == 200
@@ -31,9 +30,10 @@ class TestsSolomon:
         assert len(self.G.nodes()) == self.n_vertices + 1
 
     def test_setup_edges(self):
-        n = self.n_vertices
-        assert len(self.G.edges()) == n * (n - 1)
+        assert len(self.G.edges()) == self.n_vertices * (self.n_vertices - 1)
 
+    # These tests take too long for now, I am temporarily commenting them
+    """
     def test_subproblem_lp(self):
         self.data.solve(num_stops=4)
         assert self.data.best_value == 553.4009812212076
@@ -41,3 +41,4 @@ class TestsSolomon:
     def test_subproblem_cspy(self):
         self.data.solve(num_stops=4, cspy=True)
         assert self.data.best_value == 553.4009812212076
+    """
